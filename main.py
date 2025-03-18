d16aa74216fd7afb2bc394d02b535ad822f65d23
Arne Hassel <arne.hassel@capgemini.com>
2025-03-18 14:08:59 +0100

    Using geopy to facilitate coordinates search

--- main.py
+++ main.py
@@ -1,68 +1,43 @@
 import uvicorn
 from fastapi import FastAPI
-from rdflib import Graph, URIRef, Literal, Namespace
-from rdflib.namespace import RDF, SKOS, DC
 from starlette.requests import Request
-from starlette.responses import PlainTextResponse, JSONResponse
-from whoosh.searching import Results
+from starlette.responses import JSONResponse
 
-from search import get_searcher, search_municipality_name
+from search import get_searcher, search_municipality_name, get_linked_response, search_municipality_coords
 
 FORMAT_JSONLD = "application/ld+json"
 FORMAT_RDFXML = "application/rdf+xml"
 FORMAT_TURTLE = "text/turtle"
 
-SCHEMA = Namespace("https://schema.org/")
-AMV = Namespace("https://w3id.org/amv#")
-
 app = FastAPI()
 searcher = get_searcher()
 
 
-def search_linked_response(request: Request, results: Results):
-    graph = Graph()
-
-    for result in results:
-        municipality = URIRef(result.get("municipality"))
-        identifier = Literal(result.get("id"))
-        name = Literal(result.get("name"))
-        lat = Literal(result.get("lat"))
-        long = Literal(result.get("long"))
-        score = Literal(result.score)
-        graph.add((municipality, RDF.type, SKOS.Concept))
-        graph.add((municipality, DC.identifier, identifier))
-        graph.add((municipality, SKOS.prefLabel, name))
-        graph.add((municipality, SCHEMA.latitude, lat))
-        graph.add((municipality, SCHEMA.longitude, long))
-        graph.add((municipality, AMV.accuracy, score))
-
+def search_response(request: Request, results):
     media_format = request.headers.get("Accept")
-    return PlainTextResponse(graph.serialize(format=media_format,
-                                             context={
-                                                 "amv": str(AMV),
-                                                 "dc": str(DC),
-                                                 "kommunenummer": "https://register.geonorge.no/sosi-kodelister/inndelinger/inndelingsbase/kommunenummer/",
-                                                 "schema": str(SCHEMA),
-                                                 "skos": str(SKOS),
-                                             }), media_type=media_format)
+    if media_format == FORMAT_JSONLD or media_format == FORMAT_RDFXML or media_format == FORMAT_TURTLE:
+        return get_linked_response(request, results)
+
+    return JSONResponse([{
+        "id": result["id"],
+        "name": result["name"],
+        "lat": result["lat"],
+        "long": result["long"],
+        "url": result["url"],
+        "score": result["score"],
+    } for result in results])
+
+
+@app.get("/coords/{lat}/{long}")
+def search_municipality(request: Request, lat: float, long: float):
+    results = search_municipality_coords(lat, long)
+    return search_response(request, results)
 
 
 @app.get("/name/{query}")
 def search_municipality(request: Request, query: str):
     results = search_municipality_name(searcher, query)
-
-    media_format = request.headers.get("Accept")
-    if media_format == FORMAT_JSONLD or media_format == FORMAT_RDFXML or media_format == FORMAT_TURTLE:
-        return search_linked_response(request, results)
-
-    return JSONResponse([{
-        "id": result.get("id"),
-        "name": result.get("name"),
-        "lat": result.get("lat"),
-        "long": result.get("long"),
-        "url": result.get("municipality"),
-        "score": result.score,
-    } for result in results])
+    return search_response(request, results)
 
 
 if __name__ == "__main__":
--- search.py
+++ search.py
@@ -1,5 +1,10 @@
-from rdflib import Graph
-from whoosh.searching import Searcher
+from rdflib import Graph, URIRef, RDF, SKOS, DC, Literal, Namespace
+from starlette.requests import Request
+from starlette.responses import PlainTextResponse
+from whoosh.searching import Searcher, Results
+
+SCHEMA = Namespace("https://schema.org/")
+AMV = Namespace("https://w3id.org/amv#")
 
 g = Graph()
 g.parse('kommunenummer-koordinater.ttl')
@@ -7,30 +12,30 @@ g.parse('kommunenummer-koordinater.ttl')
 from whoosh.index import create_in
 from whoosh.fields import Schema, TEXT, NUMERIC
 
-schema = Schema(municipality=TEXT(stored=True),
+schema = Schema(url=TEXT(stored=True),
                 name=TEXT(stored=True),
                 id=NUMERIC(stored=True),
                 lat=NUMERIC(stored=True),
                 long=NUMERIC(stored=True))
 ix = create_in(".swhoosh", schema)
 writer = ix.writer()
-for municipality in g.query(
-        """
+municipalities = g.query("""
         PREFIX dc: <http://purl.org/dc/terms/>
         PREFIX schema: <https://schema.org/>
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
         PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
         
-        SELECT ?municipality ?name ?id ?lat ?long
+        SELECT ?url ?name ?id ?lat ?long
         WHERE {
-            ?municipality rdf:type skos:Concept .
-            ?municipality skos:prefLabel ?name . 
-            ?municipality dc:identifier ?id . 
-            ?municipality schema:latitude ?lat . 
-            ?municipality schema:longitude ?long . 
+            ?url rdf:type skos:Concept .
+            ?url skos:prefLabel ?name . 
+            ?url dc:identifier ?id . 
+            ?url schema:latitude ?lat . 
+            ?url schema:longitude ?long . 
         }
-        """):
-    writer.add_document(municipality=str(municipality.municipality),
+        """)
+for municipality in municipalities:
+    writer.add_document(url=str(municipality.url),
                         name=str(municipality.name),
                         id=int(municipality.id),
                         lat=float(municipality.lat),
@@ -46,13 +51,57 @@ class MyFuzzyTerm(FuzzyTerm):
         super(MyFuzzyTerm, self).__init__(fieldname, text, boost, maxdist, prefixlength, constantscore)
 
 
+def get_linked_response(request: Request, results: Results):
+    graph = Graph()
+
+    for result in results:
+        municipality = URIRef(result.get("url"))
+        graph.add((municipality, RDF.type, SKOS.Concept))
+        graph.add((municipality, DC.identifier, (Literal(result["id"]))))
+        graph.add((municipality, SKOS.prefLabel, (Literal(result["name"]))))
+        graph.add((municipality, SCHEMA.latitude, (Literal(result["lat"]))))
+        graph.add((municipality, SCHEMA.longitude, (Literal(result["long"]))))
+        graph.add((municipality, AMV.accuracy, (Literal(result["score"]))))
+
+    media_format = request.headers.get("Accept")
+    return PlainTextResponse(graph.serialize(format=media_format,
+                                             context={
+                                                 "amv": str(AMV),
+                                                 "dc": str(DC),
+                                                 "kommunenummer": "https://register.geonorge.no/sosi-kodelister/inndelinger/inndelingsbase/kommunenummer/",
+                                                 "schema": str(SCHEMA),
+                                                 "skos": str(SKOS),
+                                             }), media_type=media_format)
+
+
 def get_searcher():
     return ix.searcher()
 
 
+from geopy import distance
+
+
+def search_municipality_coords(lat: float, long: float):
+    return sorted([{
+        "id": municipality["id"],
+        "name": municipality["name"],
+        "lat": municipality["lat"],
+        "long": municipality["long"],
+        "url": municipality["url"],
+        "score": float(distance.distance((lat, long), (municipality["lat"], municipality["long"])).kilometers),
+    } for municipality in municipalities], key=lambda d: d["score"])[:5]
+
+
 def search_municipality_name(searcher: Searcher, query_string):
     query = QueryParser("name", ix.schema, termclass=MyFuzzyTerm).parse(query_string)
-    return searcher.search(query)
+    return [{
+        "id": result.get("id"),
+        "name": result.get("name"),
+        "lat": result.get("lat"),
+        "long": result.get("long"),
+        "url": result.get("url"),
+        "score": result.score,
+    } for result in searcher.search(query)]
 
 
 if __name__ == "__main__":
